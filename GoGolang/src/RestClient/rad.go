5c92d9ff18636880a02bf74ed5c0e74caa08e8eb
mukadder <hht179@gmail.com>
2018-04-21 12:51:43 -0400

    added few pointers

--- GoGolang/src/RestClient/rad.go
+++ GoGolang/src/RestClient/rad.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"runtime"
+	"time"
 )
 
 var c, python, java bool
@@ -11,6 +13,19 @@ var i, j int = 1, 2
 
 func add(x int, y int) int {
 	return x + y
+}
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
 }
 func swap(x, y string) (string, string) {
 	return y, x
@@ -23,8 +38,26 @@ func split(sum int) (x, y int) {
 	y = sum - x
 	return
 }
+func pow2(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
 
 func main() {
+	ii, jj := 42, 2701
+	p := &ii
+	fmt.Println(*p)
+	*p = 21
+	fmt.Println(ii)
+	p = &j
+	*p = *p / 37
+	fmt.Println(jj)
+
 	fmt.Println("My favorite number is ", rand.Intn(10))
 	fmt.Printf("Now you have %g problems", math.Sqrt(7))
 	fmt.Println(math.Pi)
@@ -52,4 +85,49 @@ func main() {
 	}
 	fmt.Println(sum)
 	fmt.Println(sum2)
+	fmt.Println(sqrt(2), sqrt(-4))
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+	fmt.Println(
+		pow2(3, 2, 10),
+		pow2(3, 3, 20),
+	)
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.", os)
+	}
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+	defer fmt.Println("world")
+
+	fmt.Println("hello")
+
 }
